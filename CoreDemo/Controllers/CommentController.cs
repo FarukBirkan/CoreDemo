6f03efcc279b843b6388529dfad0188c3ac5d721
FARUK UZUN <farukbirkanuzun@gmail.com>
2022-04-30 09:18:45 +0300

    Yorum ekleme yapıldı id li yapılacak ilerde

--- CoreDemo/Controllers/CommentController.cs
+++ CoreDemo/Controllers/CommentController.cs
@@ -20,22 +20,35 @@ namespace UILayer.Controllers
             return PartialView();
         }
         [HttpPost]
-        public JsonResult PartialAddComment(string Name, string Title, string Content, int id)
+        //public PartialViewResult PartialAddComment(Comment p)
+        //{
+        //    p.CommentDate = System.DateTime.Now;
+        //    p.CommentStatus = true;
+
+        //    return  PartialView();
+
+        //}
+        public JsonResult PartialAddComment(Comment p, string CommentName, string CommentTitle, string CommentContent )
         {
+            p.CommentName = CommentName;
+            p.CommentContent = CommentContent;
+            p.CommentTitle = CommentTitle;
+            p.CommentDate = System.DateTime.Now;
+            p.CommentStatus = true;
+            p.BlogId = 1;
+            //Comment cc = new Comment()
+            //{
+            //    CommentName = CommentName,
+            //    CommentTitle = CommentTitle,
+            //    CommentContent = CommentContent,
+            //    CommentDate = System.DateTime.Now,
+            //    CommentStatus = true,
+            //    BlogId = 2
+            //};
 
-            Comment cc = new Comment()
-            {
-                CommentName = Name,
-                CommentTitle = Title,
-                CommentContent = Content,
-                CommentDate = System.DateTime.Now,
-                CommentStatus = true,
-                BlogId = 2
-            };
+            cM.CommentAdd(p);
 
-            cM.CommentAdd(cc);
-
-            return Json(cc);
+            return Json(p);
 
         }
         public PartialViewResult PartialListComment(int id)
