e4a09e8022d9dc3a0cafbeeba6b3f48b6e67102e
FARUK UZUN <farukbirkanuzun@gmail.com>
2022-05-05 12:24:03 +0300

    About ve Contact yazıldı

---
+++ CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager ctm = new ContactManager(new EfContactRepository());
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Index(string ContextMail, string ContextSubject, string ContextMessage, string ContextUserName)
+        {
+
+            Contact nL = new Contact()
+            {
+                ContextUserName = ContextUserName,
+                ContextSubject = ContextSubject,
+                ContextMessage = ContextMessage,
+                ContextMail = ContextMail,
+                ContextDate = System.DateTime.Now,
+                ContextStatus = true
+            };
+
+
+            ctm.ContactAdd(nL);
+
+
+            return Json(nL);
+        }
+    }
+}
