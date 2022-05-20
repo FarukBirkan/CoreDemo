86493d77ad47ba99d73496b97f0fbfcd03bc190b
FARUK UZUN <farukbirkanuzun@gmail.com>
2022-05-20 14:13:51 +0300

    düzenleme 49.Ders

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -3,14 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace UILayer.Controllers
 {
+  
     public class WriterController : Controller
     {
-        
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
-         
-
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();  
+        }
     }
 }
